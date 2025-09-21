c468d04dd141864869ea5fe5c5d359223258ac2a
Zama15 <alexavdstiles@gmail.com>
2025-09-20 20:49:45 -0600

    Infrastructure add more Seeding

--- packages/infrastructure/seed/faculty.seed.js
+++ packages/infrastructure/seed/faculty.seed.js
@@ -8,7 +8,4 @@ export const facultySeedData = [
   {
     name: "FECAM",
   },
-  {
-    name: "EDUC",
-  },
 ];
---
+++ packages/infrastructure/seed/file.seed.js
@@ -0,0 +1,32 @@
+export const fileSeedData = [
+  {
+    storedPath:
+      "/home/zama/Documents/ReUC/packages/file-storage/storage/application-bannerTecnologia.jpg",
+    storedName: "application-bannerTecnologia.jpg",
+    originalName: "Tecnologia.jpg",
+    mimetype: "image/jpg",
+    fileSize: 69235,
+    fileKind: "image",
+    isAsset: true,
+  },
+  {
+    storedPath:
+      "/home/zama/Documents/ReUC/packages/file-storage/storage/application-bannerMedio-ambiente.png",
+    storedName: "application-bannerMedio-ambiente.png",
+    originalName: "Medio-ambiente.png",
+    mimetype: "image/png",
+    fileSize: 60792,
+    fileKind: "image",
+    isAsset: true,
+  },
+  {
+    storedPath:
+      "/home/zama/Documents/ReUC/packages/file-storage/storage/application-bannerVida-marina.jpg",
+    storedName: "application-bannerVida-marina.jpg",
+    originalName: "Vida-marina.jpg",
+    mimetype: "image/jpg",
+    fileSize: 299764,
+    fileKind: "image",
+    isAsset: true,
+  },
+];
