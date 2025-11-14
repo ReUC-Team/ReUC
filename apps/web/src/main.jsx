0cca1f02207037c2b8214db01e2b8c98b0264fd3
Cuenta Generica <confluence@clevercloud.mx>
2025-11-14 17:57:11 -0600

    Display sidebar by role

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -5,13 +5,16 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import { AccessibilityProvider } from './context/AccesibilityContext.jsx';
+import { AuthProvider}  from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AccessibilityProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AccessibilityProvider>
+    <AuthProvider>
+      <AccessibilityProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AccessibilityProvider>
+    </AuthProvider>
   </React.StrictMode>
 )
