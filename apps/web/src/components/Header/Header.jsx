9ba3568fdb64492aa14f7a1e7dc30f2ec1684da0
Cuenta Generica <confluence@clevercloud.mx>
2025-11-14 19:25:46 -0600

    General searcher

--- apps/web/src/components/Header/Header.jsx
+++ apps/web/src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+// import { useState } from 'react';
 import LogoUDC from '@/assets/logo-udc.png';
 import NotificacionDropdown from './NotificationDropdown';
 import MessageDropdown from './MessageDropdown';
 import useGetProfile from '@/features/profile/hooks/useGetProfile';
+import SearchBar from "@/features/search/components/SearchBar"; 
+
 
 export default function Header() {
-  const [showNotifications, setShowNotifications] = useState(false)
-  const [showMessage, setShowMessage] = useState(false)
+  // const [showNotifications, setShowNotifications] = useState(false)
+  // const [showMessage, setShowMessage] = useState(false)
 
   const { profile } = useGetProfile();
 
@@ -37,19 +39,12 @@ export default function Header() {
           />
       </Link>
 
-      <div className="flex items-center bg-gray-100 rounded-full px-4 py-2 w-5/12">
-        <svg className='text-4xl' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10 18a7.95 7.95 0 0 0 4.897-1.688l4.396 4.396l1.414-1.414l-4.396-4.396A7.95 7.95 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8s3.589 8 8 8m0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6s-6-2.691-6-6s2.691-6 6-6"/></svg>
-        <input
-          type="text"
-          placeholder="Buscar"
-          className="bg-transparent outline-none px-2 w-full text-sm"
-          />
-      </div>
+      <SearchBar />
       
 
       <div className="flex items-center gap-4">
 
-      <button onClick={() => setShowMessage (!showMessage)} type="button" class="relative inline-flex items-center p-3 pr-0 text-sm font-medium text-center rounded-lg cursor-pointer">
+      {/* <button onClick={() => setShowMessage (!showMessage)} type="button" class="relative inline-flex items-center p-3 pr-0 text-sm font-medium text-center rounded-lg cursor-pointer">
         <svg className='text-4xl' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 18h2v4.081L11.101 18H16c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2H4c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2"/><path fill="currentColor" d="M20 2H8c-1.103 0-2 .897-2 2h12c1.103 0 2 .897 2 2v8c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2"/></svg>
         <span class="sr-only">Notifications</span>
           <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-red-600 rounded-full -top-0 -end-3">2</div>
@@ -60,7 +55,7 @@ export default function Header() {
         
         <span className="sr-only">Notifications</span>
           <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-red-600 rounded-full top-0 -end-0">2</div>
-        </button> 
+        </button>  */}
         
           <Link 
             to="/profile"
@@ -71,8 +66,8 @@ export default function Header() {
           </Link>
       </div>
     </header>
-    {showNotifications && <NotificacionDropdown />}
-    {showMessage && <MessageDropdown />}
+    {/* {showNotifications && <NotificacionDropdown />}
+    {showMessage && <MessageDropdown />} */}
     </>
   );
 }
